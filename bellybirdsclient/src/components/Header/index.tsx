a30f98176f9009243f1487eab15a68d38da81bcc
Sai krishna reddy Mudhiganti <saikrishnareddymudhiganti@Sais-MacBook-Pro.local>
2021-02-07 15:44:16 -0500

    add logo

--- bellybirdsclient/src/components/Header/index.tsx
+++ bellybirdsclient/src/components/Header/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import './style.scss';
+import { useHistory } from "react-router-dom"
+import logo from "./bellybird.png";
 
 export default function Header() {
+  const history = useHistory();
+
+function redirectUser() {
+  history.push("/");
+}
+
   return (
     <header className="home__header">
-      <h1 className = "home__header__title">
-       BellyBirds
-      </h1>
+      <h1 className="home__header__heading">Belly Birds</h1>
+
+      <a className="home__header__logo" id="home__header__logo" href="/"  onClick={redirectUser}>
+        <img src={logo} alt="PhoenixContact"/>
+     </a>
     </header>
   )
 }
