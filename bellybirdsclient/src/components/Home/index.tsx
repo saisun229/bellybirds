286b8bb8b1a20b4bbe8de6d5d723ef5c9b7f4611
Sai krishna reddy Mudhiganti <saikrishnareddymudhiganti@Sais-MacBook-Pro.local>
2021-02-07 20:50:22 -0500

    wrkng

--- bellybirdsclient/src/components/Home/index.tsx
+++ bellybirdsclient/src/components/Home/index.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import Header from "../Header";
 import './style.scss';
-import Link  from '@material-ui/core/Link'
+import Link  from '@material-ui/core/Link';
+import { useHistory } from "react-router-dom";
 
 
 export default function Home() {
-
+  const history = useHistory()
+  function redirectUserToChat() {
+    history.push("/chat");
+  }
 
   return (
     <div className="home">
       <Header></Header>
       <div className="home__navigation">
         <h6>Home Page, Coming soon...</h6>
-        <Link component="button" href="/chat">Link to Chat</Link>
+        <Link component="button"  onClick={redirectUserToChat}>Link to Chat</Link>
       </div>
     </div>
   )
