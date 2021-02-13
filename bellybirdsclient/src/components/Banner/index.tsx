9d7e467bbe701a2ceef1e8de9f88babf52bb0d40
Sai krishna reddy Mudhiganti <saikrishnareddymudhiganti@Sais-MacBook-Pro.local>
2021-02-13 18:23:59 -0500

    add nav icons

--- bellybirdsclient/src/components/Banner/index.tsx
+++ bellybirdsclient/src/components/Banner/index.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import Header from "../Header";
 import './style.scss';
-import Link from '@material-ui/core/Link';
-import { useHistory } from "react-router-dom";
-import { store } from "../../redux";
-import Main from "../Styleguide/Main";
 import Nav from "../Nav";
 
 
