e157c577df731764c0c5a9693c45a368f124cc8c
Sai krishna reddy Mudhiganti <saikrishnareddymudhiganti@Sais-MacBook-Pro.local>
2021-02-20 17:02:22 -0500

    dropdown

--- bellybirdsclient/src/components/Home/index.tsx
+++ bellybirdsclient/src/components/Home/index.tsx
@@ -1,21 +1,10 @@
 import React from "react";
 import './style.scss';
-import Link from '@material-ui/core/Link';
-import { useHistory } from "react-router-dom";
-import { store } from "../../redux";
 import Banner from "../Banner";
 import Main from "../Styleguide/Main";
 
 
 export default function Home() {
-  const history = useHistory();
-
-  const state: any = store.getState();
-  const isUserAvailable = state.user.email ? true : false
-
-  function redirectUserToChat() {
-    isUserAvailable ? history.push("/chat") : history.push("/login");
-  }
 
   return (
     <div className="home">
@@ -23,7 +12,6 @@ export default function Home() {
       <Main>
         <div className="home__navigation">
           <h6>Home Page, Coming soon...</h6>
-          <Link component="button" onClick={redirectUserToChat}>Link to Live Chat</Link>
         </div>
       </Main>
     </div>
--- bellybirdsclient/src/components/Nav/index.tsx
+++ bellybirdsclient/src/components/Nav/index.tsx
@@ -3,35 +3,46 @@ import HomeIcon from '@material-ui/icons/Home';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import ChatIcon from '@material-ui/icons/Chat';
 import { store } from "../../redux";
-// import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-// import Link  from '@material-ui/core/Link';
-// import Typography from '@material-ui/core/Typography';
+import React, { useState }  from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from "react-router-dom";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { ClickAwayListener } from '@material-ui/core';
 
-// const useStyles = makeStyles((theme: Theme) =>
-//   createStyles({
-//     root: {
-//       '& > * + *': {
-//         marginLeft: theme.spacing(2),
-//         color: '#000'
-//       },
-//     },
-//   }),
-// );
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
 export default function Nav() {
 
   const history = useHistory();
   const state: any = store.getState();
   const isUserAvailable = state.user.email ? true : false;
-  // const classes = useStyles();
+  const classes = useStyles();
 
-  // function redirectToLogin() {
-  //   history.push("/login");
-  // }
+  const [isActive, setActive] = useState(false);
 
-  // function redirectToRegister() {
-  //   history.push("/register");
-  // }
+  const handleToggle = () => {
+    setActive(!isActive);  
+  };
+
+  const handleClickAway = () => {
+     setActive(false);
+  };
+
+  function redirectUserToLogin() {
+    history.push("/login");
+  }
+
+  function redirectUserToRegister() {
+    history.push("/register");
+  }
 
   function redirectUserToChat() {
     isUserAvailable ? history.push("/chat") : history.push("/login");
@@ -41,41 +52,52 @@ export default function Nav() {
     isUserAvailable ? history.push("/") : history.push("/login");
   }
 
-  // function logoutUser() {
-  //   window.localStorage.removeItem("token");
-  //   store.dispatch({ type: 'getUser', user: { email: "", uname: "" } });
-  //   history.push("/");
-  // }
+  function logoutUser() {
+    window.localStorage.removeItem("token");
+    store.dispatch({ type: 'getUser', user: { email: "", uname: "" } });
+    history.push("/");
+  }
 
   return (
-   <nav className="nav">
-     <div className="nav--home">
-     <HomeIcon fontSize="large" onClick={redirectUserToHome}></HomeIcon>
-     </div>
-     <div className="nav--chat">
-     <ChatIcon fontSize="large" onClick={redirectUserToChat}></ChatIcon>
-     </div>
-     <div className="nav-account">
-     <AccountCircleIcon fontSize="large"></AccountCircleIcon>
-     </div>
-     
-      {/* {
-        isUserAvailable ? (
-
-          <div className="home__header__nav">
-           <Typography className={classes.root}>
-             <Link href="#" component="button" onClick={logoutUser}>Logout</Link>
-             <Link href="#" component="button" onClick={redirectUser} color="inherit">Hi {state.user.uname}</Link>
-          </Typography>
-          </div>) : 
-          (<div className="home__header__nav">
-           <Typography className={classes.root}>
-             <Link href="#" component="button"  onClick={redirectToLogin}  >Login</Link>
-             <Link href="#" component="button"  onClick={redirectToRegister} >Register</Link>
-          </Typography>
-          </div>)
-
-      } */}
-   </nav>
+    <nav className="nav">
+      <div className="nav--home">
+        <HomeIcon fontSize="large" onClick={redirectUserToHome}></HomeIcon>
+      </div>
+      <div className="nav--chat">
+        <ChatIcon fontSize="large" onClick={redirectUserToChat}></ChatIcon>
+      </div>
+      <ClickAwayListener onClickAway={handleClickAway}>
+      <div className="nav--account">
+      
+        <AccountCircleIcon fontSize="large" onClick={handleToggle}></AccountCircleIcon>
+        <div  className={ isActive ? "nav--account-popup active" : "nav--account-popup"}>
+          {
+            isUserAvailable ? (
+            <div className={classes.root}>
+              <List component="nav" aria-label="user login logout">
+                <ListItem button>
+                  <ListItemText primary={ 'Hi ' + state.user.uname + '!'} />
+                </ListItem>
+                <ListItem button onClick={logoutUser}>
+                  <ListItemText primary="Logout"/>
+                </ListItem>
+              </List>
+            </div>) : (            
+            <div className={classes.root}>
+              <List component="nav" aria-label="user login logout">
+                <ListItem button onClick={redirectUserToLogin}>
+                  <ListItemText primary="Login" />
+                </ListItem>
+                <ListItem button onClick={redirectUserToRegister}>
+                  <ListItemText primary="Register"/>
+                </ListItem>
+              </List>
+            </div>)
+          }
+        </div>
+        
+      </div>
+      </ClickAwayListener>
+    </nav>
   )
 }
